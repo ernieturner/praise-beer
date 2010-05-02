9926f2939d4eb0675c8d96aa76e978dd6896569a
Ernie.Turner <ernie.turner@gmail.com>
2010-05-02 18:53:11 +0000

    Revamp results layout to be more fluid. Add click listeners to buttons and open up beer profile URL.

--- App/src/org/praisebeer/app/ResultsDisplay.java
+++ App/src/org/praisebeer/app/ResultsDisplay.java
@@ -1,18 +1,62 @@
 package org.praisebeer.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class ResultsDisplay extends Activity 
+public class ResultsDisplay extends Activity
 {
+	private ScanDetails scanResults;
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_results);
         Bundle extras = getIntent().getExtras();
         ScanDetails scanDetails = (ScanDetails) extras.getSerializable("scanResults");
         setResultValues(scanDetails);
+        
+        Button goToBeerProfilePage = (Button) findViewById(R.id.viewBeerPage);
+        Button goToBeerStylePage = (Button) findViewById(R.id.findOtherSimilarStyles);
+
+        goToBeerProfilePage.setOnClickListener(new Button.OnClickListener() {
+        	public void onClick(View v)
+        	{
+        		String beerProfileUrl = ResultsDisplay.this.scanResults.getProductLink();
+        		if(beerProfileUrl != "" && beerProfileUrl != null)
+        		{
+        			Intent i = new Intent(Intent.ACTION_VIEW);
+            		i.setData(Uri.parse(beerProfileUrl));
+            		startActivity(i);
+        		}
+        		else
+        		{
+        			//TODO: Handle failure state
+        		}
+            }
+        });
+        
+        goToBeerStylePage.setOnClickListener(new Button.OnClickListener() {
+        	public void onClick(View v)
+        	{
+        		/*String beerStyleUrl = ResultsDisplay.this.scanResults.getStyleLink();
+        		if(beerStyleUrl != "" && beerStyleUrl != null)
+        		{
+        			Intent i = new Intent(Intent.ACTION_VIEW);
+            		i.setData(Uri.parse(beerStyleUrl));
+            		startActivity(i);
+        		}
+        		else
+        		{
+        			//TODO: Handle failure state
+        		}*/
+            }
+        });
     }
     
     /**
@@ -20,23 +64,20 @@ public class ResultsDisplay extends Activity
 	 */
 	private void setResultValues(ScanDetails scanResults)
 	{
-        ((TextView) findViewById(R.id.beerName)).setText(scanResults.getProduct());
-        ((TextView) findViewById(R.id.beerStyle)).setText(scanResults.getBeerStyle());
-        ((TextView) findViewById(R.id.beerABV)).setText(scanResults.getBeerABV());
+		this.scanResults = scanResults;
+        ((TextView) findViewById(R.id.beerName)).setText(this.scanResults.getProduct());
+        ((TextView) findViewById(R.id.beerStyle)).setText(this.scanResults.getBeerStyle());
+        ((TextView) findViewById(R.id.beerABV)).setText(this.scanResults.getBeerABV());
 
         TextView communityRating = (TextView) findViewById(R.id.communityRating);
         TextView brothersRating = (TextView) findViewById(R.id.brothersRating);
         
-        communityRating.setText(scanResults.getCommunityRating());
-        brothersRating.setText(scanResults.getBrothersRating());
-        communityRating.setTextColor(scanResults.calculateColorByRating(scanResults.getCommunityRating()));
-        brothersRating.setTextColor(scanResults.calculateColorByRating(scanResults.getBrothersRating()));
-        ((TextView) findViewById(R.id.communityRatingName)).setText(scanResults.getCommunityRatingDescription());
-        ((TextView) findViewById(R.id.brothersRatingName)).setText(scanResults.getBrothersRatingDescription());
-        ((TextView) findViewById(R.id.communityRatingCount)).setText("w/ " + scanResults.getNumberOfRatings() + " reviews");
-        
-        //TextView beerOverviewPage = (TextView) findViewById(R.id.viewBeerPage);
-        //Linkify.addLinks(beerOverviewPage.getText, pattern, scheme, null, mentionFilter);
-        //TextView beerStylePage = (TextView) findViewById(R.id.findOtherSimilarStyles);
+        communityRating.setText(this.scanResults.getCommunityRating());
+        brothersRating.setText(this.scanResults.getBrothersRating());
+        communityRating.setTextColor(this.scanResults.calculateColorByRating(scanResults.getCommunityRating()));
+        brothersRating.setTextColor(this.scanResults.calculateColorByRating(scanResults.getBrothersRating()));
+        ((TextView) findViewById(R.id.communityRatingName)).setText(this.scanResults.getCommunityRatingDescription());
+        ((TextView) findViewById(R.id.brothersRatingName)).setText(this.scanResults.getBrothersRatingDescription());
+        ((TextView) findViewById(R.id.communityRatingCount)).setText("w/ " + this.scanResults.getNumberOfRatings() + " reviews");
 	}
 }
