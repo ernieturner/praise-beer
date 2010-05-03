c3d47bc2fee001d74c066ac4713a14721101ed91
Ernie.Turner <Ernie.Turner@371591dc-8282-971a-3f1d-ea240a309e32>
2010-05-03 02:57:08 +0000

    Converting all tabs to spaces. Adding error code handling for UPC code not found. Stemmed out initial manual entry view+activity.

--- trunk/App/src/org/praisebeer/app/FrontController.java
+++ trunk/App/src/org/praisebeer/app/FrontController.java
@@ -9,80 +9,108 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class FrontController extends Activity 
+public class FrontController extends Activity
 {
-	/** Called when the activity is first created. */
+    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
         // Obtain handles to UI objects
         Button scanBeer = (Button) findViewById(R.id.scanBeer);
         Button quitApp = (Button) findViewById(R.id.quitApp);
-        
+
         // Register handler for beer scanning
-        scanBeer.setOnClickListener(new Button.OnClickListener() {
-        	public void onClick(View v)
-        	{
-    	    	IntentIntegrator.initiateScan(FrontController.this); 
+        scanBeer.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                IntentIntegrator.initiateScan(FrontController.this);
             }
         });
 
-    	// Register handler for closing app
-        quitApp.setOnClickListener(new Button.OnClickListener() {
-        	public void onClick(View v) {
-        		finish();
+        // Register handler for closing app
+        quitApp.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                finish();
             }
         });
     }
-    
+
     /**
-     * Handles results from both the barcode scanning activity and the
-     * result lookup activity
+     * Handles results from both the barcode scanning activity and the result lookup activity
      */
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-    	//Result from bar code scan
-    	if(requestCode == IntentIntegrator.REQUEST_CODE)
-    	{
-    		if(resultCode != RESULT_CANCELED)
-    		{
-	    		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-	    		if (scanResult != null) 
-	    		{
-	    			Intent i = new Intent(this, UpcResults.class);
-	    	    	i.putExtra("upcCode", scanResult.getContents());
-	    	    	i.putExtra("upcFormat", scanResult.getFormatName());
-	    	    	startActivityForResult(i, UpcResults.REQUEST_CODE);
-	    		}
-    		}
-    		else
-    		{
-    			//TODO: Handle barcode scan cancel
-    		}
-    	}
-    	//Result from UPC lookup
-    	else if(requestCode == UpcResults.REQUEST_CODE)
-    	{
-    		if(resultCode != RESULT_CANCELED)
-    		{
-    			ScanDetails scanResults = (ScanDetails) data.getSerializableExtra("scanResults");
-    			if(scanResults != null)
-    			{
-	    			Intent i = new Intent(this, ResultsDisplay.class);
-	    			i.putExtra("scanResults", scanResults);
-	    	    	startActivity(i);
-    			}
-    			else
-    			{
-    				//TODO: Handle lack of response from UPC lookup
-    			}
-    		}
-    		else
-    		{
-    			//TODO: Handle request cancel
-    		}
-    	}
+        // Result from bar code scan
+        if(requestCode == IntentIntegrator.REQUEST_CODE)
+        {
+            if(resultCode != RESULT_CANCELED)
+            {
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                if(scanResult != null)
+                {
+                    Intent i = new Intent(this, UpcResults.class);
+                    i.putExtra("upcCode", scanResult.getContents());
+                    i.putExtra("upcFormat", scanResult.getFormatName());
+                    startActivityForResult(i, UpcResults.REQUEST_CODE);
+                }
+            }
+        }
+        // Result from UPC lookup
+        else if(requestCode == UpcResults.REQUEST_CODE)
+        {
+            if(resultCode != RESULT_CANCELED)
+            {
+                ScanDetails scanResults = (ScanDetails) data.getSerializableExtra("scanResults");
+                if(scanResults != null)
+                {
+                    if(scanResults.getScanSuccess())
+                    {
+                        Intent i = new Intent(this, ResultsDisplay.class);
+                        i.putExtra("scanResults", scanResults);
+                        startActivity(i);
+                    }
+                    else
+                    {
+                        switch(scanResults.getResultErrorCode())
+                        {
+                            case ApiErrorCodes.NO_BARCODE_RECIEVED:
+                                //Generic text error
+                                break;
+                            case ApiErrorCodes.OUTGOING_REQUEST_FAILURE:
+                                //Generic text error
+                                break;
+                            case ApiErrorCodes.INVALID_JSON_RESPONSE:
+                                //Generic text error
+                                break;
+                            case ApiErrorCodes.INVALID_UPC_CODE:
+                                //Generic text error
+                                break;
+                            case ApiErrorCodes.NO_UPC_CODE_SENT:
+                                //Generic text error
+                                break;
+                            case ApiErrorCodes.NO_UPC_FOUND:
+                                Intent i = new Intent(this, ManualUserEntry.class);
+                                i.putExtra("scanResults", scanResults);
+                                startActivity(i);
+                                break;
+                            case ApiErrorCodes.NO_BEER_FOUND:
+                                break;
+                            case ApiErrorCodes.RATING_LOOKUP_TIMEOUT:
+                                break;
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    // TODO: Handle lack of response from UPC lookup, load
+                    // generic error page?
+                }
+            }
+        }
     }
 }
--- trunk/App/src/org/praisebeer/app/ResultsDisplay.java
+++ trunk/App/src/org/praisebeer/app/ResultsDisplay.java
@@ -10,68 +10,59 @@ import android.widget.TextView;
 
 public class ResultsDisplay extends Activity
 {
-	private ScanDetails scanResults;
-	
+    private ScanDetails scanResults;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) 
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_results);
         Bundle extras = getIntent().getExtras();
-        ScanDetails scanDetails = (ScanDetails) extras.getSerializable("scanResults");
-        setResultValues(scanDetails);
-        
+        scanResults = (ScanDetails) extras.getSerializable("scanResults");
+        setResultValues();
+
         Button goToBeerProfilePage = (Button) findViewById(R.id.viewBeerPage);
         Button goToBeerStylePage = (Button) findViewById(R.id.findOtherSimilarStyles);
 
-        goToBeerProfilePage.setOnClickListener(new Button.OnClickListener() {
-        	public void onClick(View v)
-        	{
-        		String beerProfileUrl = ResultsDisplay.this.scanResults.getProductLink();
-        		if(beerProfileUrl != "" && beerProfileUrl != null)
-        		{
-        			Intent i = new Intent(Intent.ACTION_VIEW);
-            		i.setData(Uri.parse(beerProfileUrl));
-            		startActivity(i);
-        		}
-        		else
-        		{
-        			//TODO: Handle failure state
-        		}
+        goToBeerProfilePage.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                String beerProfileUrl = ResultsDisplay.this.scanResults.getProductLink();
+                if(beerProfileUrl != "" && beerProfileUrl != null)
+                {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(beerProfileUrl));
+                    startActivity(i);
+                }
             }
         });
-        
-        goToBeerStylePage.setOnClickListener(new Button.OnClickListener() {
-        	public void onClick(View v)
-        	{
-        		/*String beerStyleUrl = ResultsDisplay.this.scanResults.getStyleLink();
-        		if(beerStyleUrl != "" && beerStyleUrl != null)
-        		{
-        			Intent i = new Intent(Intent.ACTION_VIEW);
-            		i.setData(Uri.parse(beerStyleUrl));
-            		startActivity(i);
-        		}
-        		else
-        		{
-        			//TODO: Handle failure state
-        		}*/
+
+        goToBeerStylePage.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v)
+            {
+                String beerStyleID = ResultsDisplay.this.scanResults.getBeerStyleID();
+                if(beerStyleID != "")
+                {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("http://www.beeradvocate.com/beer/style/" + beerStyleID));
+                    startActivity(i);
+                }
             }
         });
     }
-    
+
     /**
-	 * Sets text on screen to value of product found and type of UPC code scanned
-	 */
-	private void setResultValues(ScanDetails scanResults)
-	{
-		this.scanResults = scanResults;
+     * Sets text on screen to value of product found and type of UPC code scanned
+     */
+    private void setResultValues()
+    {
         ((TextView) findViewById(R.id.beerName)).setText(this.scanResults.getProduct());
         ((TextView) findViewById(R.id.beerStyle)).setText(this.scanResults.getBeerStyle());
-        ((TextView) findViewById(R.id.beerABV)).setText(this.scanResults.getBeerABV());
+        ((TextView) findViewById(R.id.beerABV)).setText(this.scanResults.getBeerABV() + "%");
 
         TextView communityRating = (TextView) findViewById(R.id.communityRating);
         TextView brothersRating = (TextView) findViewById(R.id.brothersRating);
-        
+
         communityRating.setText(this.scanResults.getCommunityRating());
         brothersRating.setText(this.scanResults.getBrothersRating());
         communityRating.setTextColor(this.scanResults.calculateColorByRating(scanResults.getCommunityRating()));
@@ -79,5 +70,5 @@ public class ResultsDisplay extends Activity
         ((TextView) findViewById(R.id.communityRatingName)).setText(this.scanResults.getCommunityRatingDescription());
         ((TextView) findViewById(R.id.brothersRatingName)).setText(this.scanResults.getBrothersRatingDescription());
         ((TextView) findViewById(R.id.communityRatingCount)).setText("w/ " + this.scanResults.getNumberOfRatings() + " reviews");
-	}
+    }
 }
