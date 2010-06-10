937ead26425c79123c279b8931ddf78cb5ffbb96
Ernie.Turner <ernie.turner@gmail.com>
2010-06-10 05:17:50 +0000

    General code cleanup. Support modification requests and track them in a new table. Pull the description from BA as part of API response.

--- Api/PBUtils.py
+++ Api/PBUtils.py
@@ -19,54 +19,50 @@ from django.utils import simplejson
 
 GOOGLE_SEARCH_API_KEY = 'ABQIAAAAAlIdqGCJUyFNZYYITSwQaxQMMZlHq7uMtE8oKCK3ertxke9vYhTldmsx1t8SNWeqeFA1Cqo-hQcWhw'
 BOSS_SEARCH_API_KEY   = 'iwKJ.8nV34GERUNY2z2d3JeFQU8MzQi5vBdoAjzzcCGs175VukcNx1sfKvOlN529tImhNgZrrUE-'
-# -----------------------------------------------------------------------------
-# Scraper - This will scrape the BA site for us, or Google's cache, depending
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# Scraper - This will scrape the BA site for us, or Google's cache if BA is being too slow
+# ----------------------------------------------------------------------------------------
 class Scraper():
-
   def doScrape(self,url):
-    try:        
+    try:
       result = urlfetch.fetch(url,deadline=10)
-      if result.status_code == 200:       
-        content = re.sub('\n|\r','',result.content)
-        pat = re.compile('BAscore_big">([A-Z/]+[/]?[+-]?)</span><br>([a-z\s]+)<br>w/ ([0-9,]+) Reviews</td>.*THE BROS<br><span class="BAscore_big">([A-Z/]+[+-]?)</span><br>([a-z\s!;]+)')
-        if pat.search(content):
-          m = pat.search(content)
-          beer_info = {'ratings':{'overall':m.group(1,2,3), 'bros':m.group(4,5)}}          
-          
-          # -a-zA-Z\s()/
-          pat = re.compile('<b>Style \| ABV</b><br><a href="/beer/style/(\d+)"><b>([^<]*)</b></a> \| &nbsp;(?:(\d+\.?\d*?)%|ABV)')
-          if pat.search(content):
-            m = pat.search(content)
-            beer_info['stats'] = {'abv':m.group(3), 'style_name':m.group(2), 'style_id':m.group(1)}
-                    
-          return beer_info
+      if result.status_code == 200:
+        return self.parseResults(result.content)
+        
     except:
-      try:                              
+      try:
         cache_url = 'http://webcache.googleusercontent.com/search?q=cache:'+ url + '&hl=en&strip=1'
         result = urlfetch.fetch(cache_url,deadline=10)
-        if result.status_code == 200:         
-          content = re.sub('\n|\r','',result.content)
-          pat = re.compile('BA OVERALL<br>([A-Z/]+[+-]?)<br>([a-z\s]+)<br>w/ ([0-9,]+) Reviews</td>.*THE BROS<br>([A-Z/]+[+-]?)<br>([a-z\s!;]+)')
-          if pat.search(content):           
-            m = pat.search(content)           
-            beer_info = {'ratings':{'overall':m.group(1,2,3), 'bros':m.group(4,5)}}
-
-            pat = re.compile('<b>Style \| ABV</b><br><a href="/beer/style/(\d+)"><b>([^<]*)</b></a> \| &nbsp;(?:(\d+\.?\d*?)%|ABV)')
-            if pat.search(content):
-              m = pat.search(content)
-              beer_info['stats'] = {'abv':m.group(3), 'style_name':m.group(2), 'style_id':m.group(1)}
-            
-            return beer_info
-          else:
-            return 'No matches found.'
+        if result.status_code == 200:
+          return self.parseResults(result.content)
         else:
-          return 'An error occurred during URL fetch ('+ result.status.code +').'   
+          return 'An error occurred during URL fetch ('+ result.status.code +').'
       except:
-        return "Request timed out"  
-        
+        return "Request timed out"
     return "No results found."
-  
+
+
+  def parseResults(self, content):
+    content = re.sub('\n|\r', '', content)
+    beer_info = {}
+
+    pat = re.compile('BAscore_big">([A-Z/]+[/]?[+-]?)</span><br>([a-z\s]+)<br>w/ ([0-9,]+) Reviews</td>.*THE BROS<br><span class="BAscore_big">([A-Z/]+[+-]?)</span><br>([a-z\s!;]+)')
+    if pat.search(content):
+      m = pat.search(content)
+      beer_info['ratings'] = {'overall':m.group(1,2,3), 'bros':m.group(4,5)}
+          
+      # -a-zA-Z\s()/
+      pat = re.compile('<b>Style \| ABV</b><br><a href="/beer/style/(\d+)"><b>([^<]*)</b></a> \| &nbsp;(?:(\d+\.?\d*?)%|ABV)')
+      if pat.search(content):
+        m = pat.search(content)
+        beer_info['stats'] = {'abv':m.group(3), 'style_name':m.group(2), 'style_id':m.group(1)}
+
+        pat = re.compile('<h1 class="norm">([^<]*)<')
+        if pat.search(content):
+          m = pat.search(content)
+          beer_info['stats']['name'] = m.group(1).strip()
+                    
+    return beer_info
 # -----------------------------------------------------------------------------
 # BOSS API Search Handler Class
 # -----------------------------------------------------------------------------
